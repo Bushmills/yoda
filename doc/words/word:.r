2cc8a4b8081f25988eef6a36d6804f06b2832953
Lothar Schmidt <l@scarydevilmonastery.net>
2021-12-30 02:05:53 +0100

    more words described (open with "doc word")

---
+++ doc/words/word:.r
@@ -0,0 +1,7 @@
+name:                .r
+stack:               ( n u -- )
+return stack:        ( -- )
+string stack:        ( -- )
+category:            I/O
+decription:          outputs signed number n, padded with leading spaces to u chars.
+contexts:            interpreting, compiling
---
+++ doc/words/word:u.r
@@ -0,0 +1,7 @@
+name:                u.r
+stack:               ( u1 u2 -- )
+return stack:        ( -- )
+string stack:        ( -- )
+category:            I/O
+decription:          outputs unsigned number u1, padded with leading spaces to u2 chars.
+contexts:            interpreting, compiling

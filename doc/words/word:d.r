9407c779142245662dc5183b965a0777f4d2d0cd
L.Schmidt <l.gitkraken@scarydevilmonastery.net>
2022-02-24 19:52:20 +0100

    words: 346, compliant: 132

---
+++ doc/words/word:d.r
@@ -0,0 +1,12 @@
+name:                d.r
+stack:               ( d u -- )
+return stack:        ( -- )
+string stack:        ( -- )
+contexts:            stateless
+category:            number I/O
+
+description:         output signed double number, respecting base, right boundary
+                     in a space padded field of at least u chars wide.
+
+standard:            yes
+url:                 https://forth-standard.org/standard/core/DDotR
---
+++ doc/words/word:ud.r
@@ -0,0 +1,12 @@
+name:                ud.r
+stack:               ( ud u -- )
+return stack:        ( -- )
+string stack:        ( -- )
+contexts:            stateless
+category:            number I/O
+
+description:         output unsigned double number ud, respecting base, right boundary in
+                     a space padded field of at least u chars wide.
+
+standard:            no
+deviation:           standard has no equivalent
